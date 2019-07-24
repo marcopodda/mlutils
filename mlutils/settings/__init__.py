903859898ce79d39b0f4603ab3bd4e132086adaa
Marco Podda <marco.podda@di.unipi.it>
2019-07-24 08:40:40 +0200

    everything working

--- mlutils/settings/__init__.py
+++ mlutils/settings/__init__.py
@@ -1,6 +1,7 @@
 from mlutils.util.serialize import save_yaml
 from simple_settings import LazySettings
 
+
 def build_config(settings_file):
     settings = LazySettings('mlutils.settings.defaults', settings_file)
     save_yaml(settings.CONFIG, "config.yaml")
